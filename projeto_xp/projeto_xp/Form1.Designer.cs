aa9fe27ce3e44c21e0db2ce2e05650efec88acd7
Andre <war_frame.1@hotmail.com>
2017-02-14 14:08:18 -0200

    Vai

---
+++ projeto_xp/projeto_xp/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace projeto_xp
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Cor = new System.Windows.Forms.Button();
+            this.TT = new System.Windows.Forms.TextBox();
+            this.BoxT = new System.Windows.Forms.TextBox();
+            this.Del = new System.Windows.Forms.Button();
+            this.test = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Cor
+            // 
+            this.Cor.Location = new System.Drawing.Point(12, 72);
+            this.Cor.Name = "Cor";
+            this.Cor.Size = new System.Drawing.Size(75, 23);
+            this.Cor.TabIndex = 0;
+            this.Cor.Text = "Corfirm";
+            this.Cor.UseVisualStyleBackColor = true;
+            this.Cor.Click += new System.EventHandler(this.Cor_Click);
+            // 
+            // TT
+            // 
+            this.TT.Location = new System.Drawing.Point(12, 30);
+            this.TT.Name = "TT";
+            this.TT.Size = new System.Drawing.Size(100, 20);
+            this.TT.TabIndex = 1;
+            // 
+            // BoxT
+            // 
+            this.BoxT.Location = new System.Drawing.Point(143, 30);
+            this.BoxT.Multiline = true;
+            this.BoxT.Name = "BoxT";
+            this.BoxT.Size = new System.Drawing.Size(100, 114);
+            this.BoxT.TabIndex = 2;
+            // 
+            // Del
+            // 
+            this.Del.Location = new System.Drawing.Point(12, 101);
+            this.Del.Name = "Del";
+            this.Del.Size = new System.Drawing.Size(57, 24);
+            this.Del.TabIndex = 3;
+            this.Del.Text = "Delete";
+            this.Del.UseVisualStyleBackColor = true;
+            this.Del.Click += new System.EventHandler(this.Del_Click);
+            // 
+            // test
+            // 
+            this.test.Location = new System.Drawing.Point(207, 1);
+            this.test.Name = "test";
+            this.test.Size = new System.Drawing.Size(36, 23);
+            this.test.TabIndex = 4;
+            this.test.Text = "test";
+            this.test.UseVisualStyleBackColor = true;
+            this.test.Click += new System.EventHandler(this.test_Click);
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(255, 157);
+            this.Controls.Add(this.test);
+            this.Controls.Add(this.Del);
+            this.Controls.Add(this.BoxT);
+            this.Controls.Add(this.TT);
+            this.Controls.Add(this.Cor);
+            this.Name = "Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Projeto XP";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Cor;
+        private System.Windows.Forms.TextBox TT;
+        private System.Windows.Forms.TextBox BoxT;
+        private System.Windows.Forms.Button Del;
+        private System.Windows.Forms.Button test;
+    }
+}
+
---
+++ projeto_xp/projeto_xp/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace projeto_xp
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public string[] values = new string[10];
+        public int i = 0;
+
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        private void Cor_Click(object sender, EventArgs e)
+        {
+
+
+            if (TT.Text == String.Empty || i <= 9)
+            { values[i] = TT.Text; TT.Text = String.Empty; TT.Focus(); i++; }
+
+            BoxT.Text = String.Empty;
+            TT.Focus();
+
+            for (int x = 9; x >= 0; x--)
+            {
+                if (values[x] != null)
+                { BoxT.Text += values[x] + System.Environment.NewLine; }
+            }
+        }
+        
+
+        private void Del_Click(object sender, EventArgs e)
+        {
+            values[i] = null;
+            i--;
+
+            BoxT.Text = String.Empty;
+            TT.Focus();
+
+            for (int x = 9; x >= 0; x--) {
+                if (values[x] != null) { BoxT.Text += values[i] + System.Environment.NewLine; }
+            }
+        }
+
+        private void test_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(values[9]);
+        }
+    }
+}
